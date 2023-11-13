ae48eb07fa9715991e337a22e97658ef627e33d7
pdotti12 <141247791+pdotti12@users.noreply.github.com>
2023-11-13 10:58:15 -0300

    Assinaturas e Clientes

--- src/Cliente.java
+++ src/Cliente.java
@@ -7,4 +7,48 @@ public class Cliente {
 
     private List<Assinatura> assinaturas;
 
+    
+    public Cliente(String nome, String cpf, String email, List<Assinatura> assinaturas) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.assinaturas = assinaturas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Assinatura> getAssinaturas() {
+        return assinaturas;
+    }
+
+    public void setAssinaturas(List<Assinatura> assinaturas) {
+        this.assinaturas = assinaturas;
+    }
+
+    public void cancelaAssinatura(Assinatura assinatura){
+        assinaturas.remove(assinatura.getCodigoAssinatura());
+    }
+
 }
